7ed2ae8a237a95c54d7c2237df4502bd1ac52528
Markus Schanta <markus@schanta.at>
2018-11-20 20:38:24 +0000

    Use requests instead of urllib

--- mslib/data/bitcoin/coinmarketcap.py
+++ mslib/data/bitcoin/coinmarketcap.py
@@ -1,14 +1,13 @@
 import json
 import time
-import urllib.request
+import requests
 
 import pandas as pd
 
 
 def get_coin_list():
     list_url = "https://s2.coinmarketcap.com/generated/search/quick_search.json"
-    with urllib.request.urlopen(list_url) as url:
-        data = json.loads(url.read().decode())
+    data = requests.get(list_url).json()
     coin_list = pd.DataFrame(data).set_index('rank').loc[:, ('slug', 'symbol', 'name')]
     return coin_list
 
@@ -17,8 +16,7 @@ def get_market_data_single(slug, resample='d'):
     market_data_url = "https://graphs2.coinmarketcap.com/currencies/%s/" % slug
     print("Fetching market data for: %s" % slug)
     time.sleep(1.5)
-    with urllib.request.urlopen(market_data_url) as url:
-        data = json.loads(url.read().decode())
+    data = requests.get(market_data_url).json()
     market_data = pd.concat({k: pd.Series(dict(v)) for k, v in data.items()}, axis=1)
     market_data.index = pd.to_datetime(list(market_data.index),unit='ms')
     if resample:
